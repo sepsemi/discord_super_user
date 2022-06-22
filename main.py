190a7cca8e6605e690572ed43c79ab538cd3a255
sepsemi <sepsemi@protonmail.com>
2022-06-23 01:49:45 +0200

    added more functionality

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import json
 import argparse
 import requests
+from datetime import datetime
 
 class DiscordRelationship:
     __slots__= ('id', 'type', 'nickname', 'username', 'discriminator', 'avatar', 'flags')
@@ -20,10 +21,12 @@ class DiscordRelationship:
         return "{}#{}".format(self.username, self.discriminator)
 
 class DiscordRequests:
+    API_VERSION = 9
+    URI = 'https://discord.com/api/v{}'.format(API_VERSION)
     
     def __init__(self, token):
         self.token = token
-        self.session = requests.Session()
+        self._session = requests.Session()
         self._guilds = set()
         self._relationships = set()
 
@@ -39,7 +42,7 @@ class DiscordRequests:
         headers['content-type'] = 'application/json'
 
         url = 'https://discord.com/api/v9/users/@me/relationships'
-        resp = self.session.get(headers=self.headers,url=url)
+        resp = self._session.get(headers=self.headers,url=url)
         
         # Check before we do anything
         if resp.status_code == 200:
@@ -49,6 +52,13 @@ class DiscordRequests:
 
     def _get_guilds(self):
         pass
+
+    def send_friend_request(self, user_id):
+        headers = self.headers.copy()
+        headers['content-type'] = 'application/json'
+
+        url = self.URI + '/users/@me/relationships/{}'.format(user_id)
+        self._session.put(headers=headers, url=url, json={})
 
     @property
     def relationships(self):
@@ -62,11 +72,13 @@ class DiscordSuperuser:
         self._parser = argparse.ArgumentParser(requester, description)
         self._parser.add_argument('--backup', metavar='filename.json', type=str, help='Backup client\'s relationships into a json formated file')
         self._parser.add_argument('--import', metavar='filename.json', type=str, dest='import_from_backup', help='import client\'s relationships from a file by sending friend requests')
-        self._parser.add_argument('--mute-guilds', type=lambda x: (str(x).lower() == 'true'), choices=(True, False), nargs='?' , help='Mutes all guilds client belongs to')
+        self._parser.add_argument('--mute-guilds', action='store_true', help='Mutes all guilds client belongs to')
         self._parser.add_argument('--list-relationships', action='store_true', help='Lists client\'s relationships')
 
         # Compile all the args into a Namespace
         self._arguments = self._parser.parse_args()
+        print(self._arguments)
+        self._requester = requester
 
     def backup_relationships(self, path):
         if os.path.exists(path):
@@ -76,7 +88,7 @@ class DiscordSuperuser:
         # Write relationships to specified path in json format
         serialized_relationships = []
         with open(path, 'w') as fp:
-            for relationship in requester.relationships:
+            for relationship in self._requester.relationships:
                 serialized_relationships.append({
                     'id': relationship.id,
                     'type': relationship.type,
@@ -85,35 +97,46 @@ class DiscordSuperuser:
 
             # write the list of serialized relationships
             fp.write(json.dumps(serialized_relationships, ensure_ascii=False, indent=4))
+
+            print('[{}][backup_relationships] sucessfully backed up ({}) relationships)'.format(datetime.now(), len(serialized_relationships)))
     
     def import_from_backup(self, path):
         # Check if the file exists
         if not os.path.exists(path):
             raise FileNotFoundError('The backup file ({}) to import from was not found'.format(path))
         
-        with open(path, 'w') as fp:
-            data = json.loads(fp)
+        relationships = []
+        with open(path, 'r') as fp:
+            json_data = json.load(fp)
+
+            for relationship in json_data:
+                # This is unsafe this assumes no one is added and will likely trigger a ban.
+                print('[{}][import_from_backup] sending friend request to user known as ({}, {})'.format(
+                    datetime.now(), relationship['id'], relationship['fullname'])
+                )
+                self._requester.send_friend_request(relationship['id'])
         
     def mute_guilds(self):
-        pass
+        raise NotImplementedError('mute_guilds is not availible in this version yet')
     
     def list_relationships(self):
-        pass
+        for relationship in self._requester.relationships:
+            print("{}, {}".format(relationship.id, relationship.name)) 
 
     def run(self):
         # run the base magic here
         if self._arguments.backup:
+
           self.backup_relationships(self._arguments.backup)
         
         if self._arguments.import_from_backup:
             self.import_from_backup(self._arguments.import_from_backup)
 
         if self._arguments.mute_guilds:
-            pass
+            self.mute_guilds()
 
         if self._arguments.list_relationships:
-            pass
-
+            self.list_relationships()
 
 
 token = os.environ.get('TOKEN')
